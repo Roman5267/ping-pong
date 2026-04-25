1966dd0cf79af0a1554079102268e4a7adc3a773
Roman5267 <boyarskyrom@yandex.ru>
2026-04-25 19:55:55 +0500

    Add files via upload

---
+++ pingpong.py
@@ -0,0 +1,70 @@
+from pygame import *
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, width, height):
+        sprite.Sprite.__init__(self)
+        self.image = transform.scale(image.load(player_image), (width, height))
+        self.speed = player_speed
+        # rect
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+
+        
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y)) 
+
+
+class Player(GameSprite):
+    def update_r(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys_pressed[K_DOWN] and self.rect.y < win_height - 150:
+            self.rect.y += self.speed
+
+    def update_l(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys_pressed[K_s] and self.rect.y < win_height - 150:
+            self.rect.y += self.speed
+
+
+
+back = (200, 255, 255)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+game = True 
+finish = False
+clock = time.Clock()
+FPS = 60
+
+
+racket1 = Player('racket.png', 30, 200, 4, 50, 150)
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+
+while game:
+    for e  in event.get():
+        if e.type == QUIT:
+            game = False
+
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+
+
+    display.update()
+    clock.tick(FPS)
+
+
